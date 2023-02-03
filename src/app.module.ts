bce0f73ba224bb4d845a861419d7361ceba5fb45
Guuhp <lg645471@gmail.com>
2023-02-03 04:43:23 -0400

    feat(Mailer) implement envio de emails com Mailer

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,36 @@
-import { MiddlewareConsumer, Module, NestModule, RequestMethod, forwardRef } from '@nestjs/common';
-
-import { UserModule } from './user/user.module';
-import { UserIdCheckMiddlewares } from './middlewares/use-id-check.middleware';
-import { AuthService } from './auth/auth.service';
-import { AuthModule } from './auth/auth.module';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { PugAdapter } from '@nestjs-modules/mailer/dist/adapters/pug.adapter';
+import { forwardRef, MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+
+import { AuthModule } from './auth/auth.module';
+import { UserIdCheckMiddlewares } from './middlewares/use-id-check.middleware';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    forwardRef(() => UserModule), 
+    MailerModule.forRoot({
+      transport: {
+        host: 'smtp.ethereal.email',
+        port: 587,
+        auth: {
+          user: 'abagail.mitchell95@ethereal.email',
+          pass: 'NCzsBK65H4hhJdmBSr'
+        }
+      },
+      defaults: {
+        from: '"luiz" <	abagail.mitchell95@ethereal.email>'
+      },
+      template: {
+        dir: __dirname + "/templates",
+        adapter: new PugAdapter(),
+        options: {
+          strict: true
+        }
+      }
+    }),
+    forwardRef(() => UserModule),
     forwardRef(() => AuthModule)
   ],
   controllers: [],
@@ -18,8 +39,8 @@ import { ConfigModule } from '@nestjs/config';
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
     consumer.apply(UserIdCheckMiddlewares).forRoutes({
-      path:'users/:id',
-      method:RequestMethod.ALL
+      path: 'users/:id',
+      method: RequestMethod.ALL
     })
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,122 +1,158 @@
-
-
-import { BadRequestException, Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { AuthRegisterDTO } from './dto/auth-register.dto';
 import { JwtService } from '@nestjs/jwt';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { UserService } from 'src/user/user.service';
 import { User } from '@prisma/client';
 import * as bcrypt from 'bcrypt';
+import { MailerService } from '@nestjs-modules/mailer';
 
 @Injectable()
 export class AuthService {
-
   constructor(
-    private jwtService: JwtService,
-    private prisma: PrismaService,
-    private userService: UserService,
+    private readonly jwtService: JwtService,
+    private readonly prisma: PrismaService,
+    private readonly userService: UserService,
+    private readonly mail: MailerService,
   ) { }
-
 
   createToken(user: User) {
     return {
-      accessToken: this.jwtService.sign({
-        //a quem pertence o token
-        id: user.id,
-        name: user.name,
-        email: user.email
-      },
+      accessToken: this.jwtService.sign(
         {
-          expiresIn: "7 days",
+          //a quem pertence o token
+          id: user.id,
+          name: user.name,
+          email: user.email,
+        },
+        {
+          expiresIn: '7 days',
           subject: String(user.id),
           //quem ta emitindo o token
           issuer: 'login',
-          audience: 'users'
-        })
-    }
+          audience: 'users',
+        },
+      ),
+    };
   }
 
   checkToken(token: string) {
     try {
       const data = this.jwtService.verify(token, {
         audience: 'users',
-        issuer: 'login'
-      })
+        issuer: 'login',
+      });
 
       return data;
     } catch (e) {
-      throw new BadRequestException(e)
+      throw new BadRequestException(e);
     }
   }
 
   isValidToken(token: string) {
     try {
-      this.checkToken(token)
-      return true
+      this.checkToken(token);
+      return true;
     } catch (e) {
-      return false
+      return false;
     }
   }
-
 
   async login(email: string, password: string) {
     const user = await this.prisma.user.findFirst({
       where: {
-        email
-      }
-    })
+        email,
+      },
+    });
     console.log(user);
 
     if (!user) {
       throw new UnauthorizedException(
-        "user not found, check email and password"
-      )
+        'user not found, check email and password',
+      );
     }
     //informação e eu quero comparar / a que esta em hash
-    if (!await bcrypt.compare(password, user.password)) {
+    if (!(await bcrypt.compare(password, user.password))) {
       throw new UnauthorizedException(
-        "user not found, check email and password"
-      )
+        'user not found, check email and password',
+      );
     }
 
-    return this.createToken(user)
+    return this.createToken(user);
   }
 
   async forget(email: string) {
     const user = await this.prisma.user.findFirst({
       where: {
-        email
-      }
-    })
+        email,
+      },
+    });
 
     if (!user) {
-      throw new UnauthorizedException("email incorrect")
+      throw new UnauthorizedException('email incorrect');
     }
+    const token = await this.jwtService.sign(
+      {
+        //a quem pertence o token
+        id: user.id,
+      },
+      {
+        expiresIn: '30 minutes',
+        subject: String(user.id),
+        //quem ta emitindo o token
+        issuer: 'forget',
+        audience: 'users',
+      },
+    );
     //enviar o email
+    await this.mail.sendMail({
+      subject: 'recuperação de senha',
+      to: email,
+      template: 'forget',
+      //variaveis do templete
+      context: {
+        name: user.name,
+        token: token,
+      },
+    });
     return true;
   }
 
   async reset(password: string, token: string) {
-    //todo se validar o token 
-    //troca a senha do user
-    const id = 0
-    const user = await this.prisma.user.update({
-      where: {
-        id
-      },
-      data: {
-        password
+    try {
+      //todo se validar o token
+      const data = await this.jwtService.verify(token, {
+        issuer: 'forget',
+        audience: 'users',
+      });
+      console.log(data);
+
+      if (isNaN(Number(data.id))) {
+        throw new BadRequestException("o token é invalido")
       }
-    })
-    return this.createToken(user);
+      const salt = await bcrypt.genSalt()
+      password = await bcrypt.hash(password, salt)
+      //troca a senha do user
+      const user = await this.prisma.user.update({
+        where: {
+          id: Number(data.id),
+        },
+        data: {
+          password,
+        },
+      });
+      return this.createToken(user);
+    } catch (error) {
+      throw new BadRequestException(error)
+    }
   }
 
   async register(data: AuthRegisterDTO) {
-    const user = await this.userService.createUser(data)
-    return this.createToken(user)
+    const user = await this.userService.createUser(data);
+    return this.createToken(user);
   }
 }
-
-
-
-
